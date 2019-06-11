9905cf62dedd7cb4670252b1857e94d8e2081c8a
Henrik Jonsson <me@hkjn.me>
2019-06-11 17:32:39 +0200

    armbian: Add more debug logging

--- armbian/build.sh
+++ armbian/build.sh
@@ -49,6 +49,7 @@ case ${ACTION} in
 			BUILD_ARGS="${BUILD_ARGS} CLEAN_LEVEL=oldcache PROGRESS_LOG_TO_FILE=yes"
 		fi
 		time ./compile.sh ${BUILD_ARGS}
+		echo "I am at $(pwd) and have output: $(ls -R output/)"
 		sha256sum output/images/Armbian_*.img
 		;;
 esac
