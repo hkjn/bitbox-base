4554a66aec78169dedae0276fdb5edd5198a80b4
Henrik Jonsson <me@hkjn.me>
2019-06-21 15:21:43 +0200

    scripts: Fix path to armbian/build

--- scripts/move-armbian-image.sh
+++ scripts/move-armbian-image.sh
@@ -9,7 +9,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # TODO(hkjn): Consider defining these config values somewhere higher-level than inside this script.
 KERNEL_VERSION="${KERNEL_VERSION:-4.4.176}"
 ARMBIAN_VERSION="${ARMBIAN_VERSION:-5.77}"
-IMG_FILE="${SCRIPT_DIR}/../armbian/armbian-build/output/images/Armbian_${ARMBIAN_VERSION}_Rockpro64_Debian_stretch_default_${KERNEL_VERSION}.img"
+IMG_FILE="${SCRIPT_DIR}/../armbian/build/output/images/Armbian_${ARMBIAN_VERSION}_Rockpro64_Debian_stretch_default_${KERNEL_VERSION}.img"
 
 if [[ ! -e "${IMG_FILE}" ]]; then
   echo "fatal: no Armbian image exists at expected path:" >&2
